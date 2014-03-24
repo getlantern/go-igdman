2de59155c936cfdd2ae694c7282cbb9231ed8f47
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-24 07:45:31 -0500

    Added ability to add/remove port mappings with upnpc

---
+++ igdman/upnp.go
@@ -0,0 +1,119 @@
+package igdman
+
+import (
+	"fmt"
+	"github.com/oxtoacart/byteexec"
+	"strings"
+	"time"
+)
+
+const (
+	IGD_URL_LABEL             = "Found valid IGD : "
+	LOCAL_IP_ADDRESS_LABEL    = "Local LAN ip address : "
+	EXTERNAL_IP_ADDRESS_LABEL = "ExternalIPAddress = "
+)
+
+type UPNPIGD struct {
+	upnpc      *byteexec.ByteExec
+	igdUrl     string
+	internalIP string
+	externalIP string
+}
+
+func NewUPNPIGD() (igd *UPNPIGD, err error) {
+	upnpcBytes, err := Asset("upnpc")
+	if err != nil {
+		return nil, err
+	}
+	be, err := byteexec.NewByteExec(upnpcBytes)
+	if err != nil {
+		return nil, err
+	}
+	return &UPNPIGD{upnpc: be}, nil
+}
+
+func (igd *UPNPIGD) GetExternalIP() (ip string, err error) {
+	err = igd.updateStatus()
+	if err != nil {
+		return "", err
+	}
+	return igd.externalIP, nil
+}
+
+func (igd *UPNPIGD) AddPortMapping(proto protocol, internalIP string, internalPort int, externalPort int, duration time.Duration) error {
+	if err := igd.updateStatus(); err != nil {
+		return fmt.Errorf("Unable to add port mapping: %s", err)
+	}
+	params := []string{
+		"-url", igd.igdUrl,
+		"-a", internalIP, fmt.Sprintf("%d", internalPort), fmt.Sprintf("%d", externalPort), string(proto),
+	}
+	if duration > 0 {
+		params = append(params, fmt.Sprintf("%d", duration/time.Second))
+	}
+	out, err := igd.upnpc.Command(params...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Unable to add port mapping: %s\n%s", err, out)
+	} else {
+		return nil
+	}
+}
+
+func (igd *UPNPIGD) RemovePortMapping(proto protocol, externalPort int) error {
+	if err := igd.updateStatus(); err != nil {
+		return fmt.Errorf("Unable to add port mapping: %s", err)
+	}
+	params := []string{
+		"-url", igd.igdUrl,
+		"-d", fmt.Sprintf("%d", externalPort), string(proto),
+	}
+	out, err := igd.upnpc.Command(params...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Unable to remove port mapping: %s\n%s", err, out)
+	} else {
+		return nil
+	}
+}
+
+// updateStatus updates the IGD's status fields
+func (igd *UPNPIGD) updateStatus() error {
+	skipDiscovery := igd.igdUrl != ""
+	params := []string{"-s"}
+	if skipDiscovery {
+		params = []string{"-url", igd.igdUrl, "-s"} // -s has to be at the end for some reason
+	}
+	out, err := igd.upnpc.Command(params...).CombinedOutput()
+	if err != nil {
+		if skipDiscovery {
+			// Clear remembered url and try again
+			igd.igdUrl = ""
+			return igd.updateStatus()
+		} else {
+			return fmt.Errorf("Unable to call upnpc to get status: %s\n%s", err, out)
+		}
+	}
+	resp := string(out)
+	if igd.igdUrl, err = extractFromStatusResponse(resp, IGD_URL_LABEL); err != nil {
+		return err
+	}
+	if igd.internalIP, err = extractFromStatusResponse(resp, LOCAL_IP_ADDRESS_LABEL); err != nil {
+		return err
+	}
+	if igd.externalIP, err = extractFromStatusResponse(resp, EXTERNAL_IP_ADDRESS_LABEL); err != nil {
+		return err
+	}
+	return nil
+}
+
+func extractFromStatusResponse(resp string, label string) (string, error) {
+	i := strings.Index(resp, label)
+	if i < 0 {
+		return "", fmt.Errorf("%s not available from upnpc", label)
+	}
+	resp = resp[i+len(label):]
+	s := strings.Index(resp, "\n")
+	if s < 0 {
+		return "", fmt.Errorf("Unable to find newline after %s", label)
+	}
+	return resp[:s], nil
+}
---
+++ igdman/upnp_test.go
@@ -0,0 +1,127 @@
+package igdman
+
+import (
+	"fmt"
+	"github.com/oxtoacart/framed"
+	"net"
+	"os"
+	"testing"
+	"time"
+)
+
+var (
+	PUBLIC_IP = "PUBLIC_IP"
+)
+
+// TestExternalIP only works when there is a valid IGD device with an external
+// IP.  The environment variable EXTERNAL_IP needs to be set for this test to
+// work.
+func TestExternalIP(t *testing.T) {
+	expectedExternalIP := os.Getenv("EXTERNAL_IP")
+	if expectedExternalIP == "" {
+		t.Fatalf("Please set the environment variable EXTERNAL_IP to provide your expected public IP address")
+	}
+
+	igd, err := NewUPNPIGD()
+	if err != nil {
+		t.Fatalf("Unable to create IGD: %s", err)
+	}
+	start := time.Now()
+	publicIp, err := igd.GetExternalIP()
+	if err != nil {
+		t.Fatalf("Unable to get Public IP: %s", err)
+	}
+	delta1 := time.Now().Sub(start)
+	if publicIp != expectedExternalIP {
+		t.Errorf("External ip '%s' did not match expected '%s'", publicIp, expectedExternalIP)
+	}
+	start = time.Now()
+	publicIp, err = igd.GetExternalIP()
+	if err != nil {
+		t.Fatalf("Unable to get External IP 2nd time: %s", err)
+	}
+	delta2 := time.Now().Sub(start)
+	if delta2 > delta1/10 {
+		t.Fatalf("2nd external ip lookup should have been much faster than first because of cached IGD url.  1st lookup: %d ms, 2nd lookup: %d ms", delta1/time.Millisecond, delta2/time.Millisecond)
+	}
+}
+
+func TestMapping(t *testing.T) {
+	port := 15067
+
+	igd, err := NewUPNPIGD()
+	if err != nil {
+		t.Fatalf("Unable to create IGD: %s", err)
+	}
+
+	externalIP, err := igd.GetExternalIP()
+	if err != nil {
+		t.Fatalf("Unable to get external IP: %s", err)
+	}
+
+	// Run echo server
+	go func() {
+		l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+		if err != nil {
+			t.Fatalf("Unable to start server: %s", err)
+		}
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				t.Fatalf("Unable to accept connection: %s", err)
+			}
+			f := framed.Framed{conn}
+			defer f.Close()
+			frame := make([]byte, 1000)
+			for {
+				n, err := f.Read(frame)
+				if err != nil {
+					return
+				}
+				_, err = f.Write(frame[:n])
+				if err != nil {
+					return
+				}
+			}
+		}
+	}()
+
+	// Add port mapping
+	err = igd.AddPortMapping(TCP, igd.internalIP, port, port, 0)
+	if err != nil {
+		t.Fatalf("Unable to add port mapping: %s", err)
+	}
+
+	// Run echo client
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", externalIP, port))
+	if err != nil {
+		t.Fatalf("Unable to connect to echo server")
+	}
+	f := framed.Framed{conn}
+	defer f.Close()
+	testString := "Hello strange port mapped world"
+	_, err = f.Write([]byte(testString))
+	if err != nil {
+		t.Fatalf("Unable to write to echo server")
+	}
+	frame := make([]byte, 1000)
+	n, err := f.Read(frame)
+	if err != nil {
+		t.Fatalf("Unable to read from echo server")
+	}
+	response := string(frame[:n])
+	if response != testString {
+		t.Errorf("Response '%s' did not match expected value '%s'", response, testString)
+	}
+
+	// Remove port mapping
+	err = igd.RemovePortMapping(TCP, port)
+	time.Sleep(2000)
+	if err != nil {
+		t.Fatalf("Unable to remove port mapping: %s", err)
+	}
+	conn, err = net.Dial("tcp", fmt.Sprintf("%s:%d", externalIP, port))
+	if err == nil {
+		t.Errorf("Connecting to address with closed port mapping should have resulted in an error")
+	}
+}
